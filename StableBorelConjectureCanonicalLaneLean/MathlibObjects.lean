0ff17424c30ac70b1636180e0509350322d2e3de
HautevilleHouse <239203806+HautevilleHouse@users.noreply.github.com>
2026-07-07 06:35:41 -0400

    Canon

--- StableBorelConjectureCanonicalLaneLean/MathlibObjects.lean
+++ StableBorelConjectureCanonicalLaneLean/MathlibObjects.lean
@@ -1,11 +1,18 @@
 import StableBorelConjectureCanonicalLaneLean.TheoremStatement
 import CanonicalLaneMathlibCore
 import Mathlib.Data.Set.Basic
+import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.Topology.Basic
 
 namespace HautevilleHouse
 namespace StableBorelConjectureCanonicalLaneLean
 
 open HautevilleHouse.CanonicalLaneMathlibCore
+
+universe u v w
+
+def MathlibCategoryGate (C : Type u) : Prop :=
+  Nonempty (CategoryTheory.Category.{u, u} C)
 
 structure TheoremSpecificObject where
   sourceKey : String
@@ -13,15 +20,34 @@ structure TheoremSpecificObject where
   claimBoundary : String
 deriving Repr, DecidableEq
 
+structure UpstreamMathlibSubstrate where
+  categoryCarrier : Type u
+  categoryGate : Prop
+  topologicalCarrier : Type v
+  topologicalStructure : TopologicalSpace topologicalCarrier
+  controlledObjects : Set String
+  assemblyOrRigidityGate : Prop
+  sourceIndexedBridge : Prop
+
 structure AdmittedTheoremObject where
   object : TheoremSpecificObject
+  substrate : UpstreamMathlibSubstrate
   localWitness : String
   bridgeEvidence : String
+  controlledAssemblyChecked : Prop
+  controlledAssemblyWitness : controlledAssemblyChecked
+  topologicalBridgeChecked : Prop
+  topologicalBridgeWitness : topologicalBridgeChecked
+  sourceIndexedBridgeChecked : Prop
+  sourceIndexedBridgeWitness : sourceIndexedBridgeChecked
+  classicalRemainderCarried : formalizationCertificate.sourceConjectureClosureClaimed = false
   sourceKeyChecked : object.sourceKey = sourceRepository
   theoremObjectChecked : object.theoremObject = sourceDescription
 
-structure EndgameState where
+structure ClosureState where
   object : AdmittedTheoremObject
+  endpointSatisfied : Prop
+  remainderRecorded : Prop
 
 def theoremSpecificObject : TheoremSpecificObject := {
   sourceKey := sourceRepository,
@@ -30,7 +56,15 @@ def theoremSpecificObject : TheoremSpecificObject := {
 }
 
 def NativeBridgeClosed (O : AdmittedTheoremObject) : Prop :=
-  O.object.sourceKey = sourceRepository ∧ O.object.theoremObject = sourceDescription
+  O.object.sourceKey = sourceRepository ∧
+  O.object.theoremObject = sourceDescription ∧
+  O.controlledAssemblyChecked ∧
+  O.topologicalBridgeChecked ∧
+  O.sourceIndexedBridgeChecked ∧
+  (formalizationCertificate.sourceConjectureClosureClaimed = false)
+
+def ScopedClosure (O : AdmittedTheoremObject) : Prop :=
+  NativeBridgeClosed O
 
 end StableBorelConjectureCanonicalLaneLean
 end HautevilleHouse
